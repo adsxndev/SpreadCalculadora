45fc70eba5079dbc7b362d19fb0aa58423b4def6
adsondev <adsondev@hotmail.com>
2025-05-12 01:18:50 -0300

    atualizando banco de dados

--- spreadsite/settings.py
+++ spreadsite/settings.py
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Segurança
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
+ALLOWED_HOSTS = ['*']
+
 
 # Aplicativos
 INSTALLED_APPS = [
