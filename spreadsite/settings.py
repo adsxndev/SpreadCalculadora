68f50d20308075195367e96455571cba9726a8a5
Adson Soares <adsondev@hotmail.com>
2025-05-28 22:42:39 -0300

    Atualização 0.5

--- spreadsite/settings.py
+++ spreadsite/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from decouple import config, Csv
+import dj_database_url
 
 # Caminhos
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,14 +54,7 @@ WSGI_APPLICATION = 'spreadsite.wsgi.application'
 
 # Banco de dados
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 # Validação de senhas
